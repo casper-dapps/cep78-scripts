ef7aa757a3741f79f239e6bc9da0aa2343bccfd1
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-10-06 05:42:31 +0900

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('@types/prettier').RequiredOptions}
+ */
+module.exports = {
+  trailingComma: 'all',
+  printWidth: 80,
+  arrowParens: 'always',
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  endOfLine: 'auto',
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# Casper CEP78 scripts
